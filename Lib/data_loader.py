d6874451e26cf385508c07245f757d85aee7c00c
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-07-10 18:40:06 +0100

    Adding data loader class that is context class for loading strategy pattern

---
+++ Lib/data_loader.py
@@ -0,0 +1,14 @@
+
+class dataLoader():
+    """
+    Context class for loading data.
+    Has strategies derived from fileLoader, webLoader classes. 
+    See readme.md for more info.
+
+    """
+
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    def get_data(self):
+        return self._strategy.get_data()
