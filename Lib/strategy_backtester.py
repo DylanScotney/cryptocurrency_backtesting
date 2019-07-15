21f1284f6f4b62b95cb04d511b6f6a653e31ffa2
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-07-15 17:16:27 +0100

    adding docstrings to backtest class

--- Lib/strategy_backtester.py
+++ Lib/strategy_backtester.py
@@ -1,11 +1,24 @@
 
 class backtest():
+    """
+    Context class for implementing trading strategies. 
+    
+    Construction:
+    - strategy:             Trading strategy class such as:
+                            crossoverTrading, zScoreTrading. 
+    - plot_results:         bool to indicate whether to plot trading
+                            activity.
+    """
 
     def __init__(self, strategy, plot_results=False):
         self._strategy = strategy
         self.plot_results = False
 
     def trade(self):
+        """
+        Implements the stategies trading method. 
+        """
+        
         self._strategy.trade(plot=self.plot_results)
 
     
