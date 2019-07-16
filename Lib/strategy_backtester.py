89346b11d91379ccf3253eaf0416fc56b524c91d
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-07-16 12:55:43 +0100

    PEP8 formatting strategy_backtester.py

--- Lib/strategy_backtester.py
+++ Lib/strategy_backtester.py
@@ -1,21 +1,21 @@
 
 class backtest():
     """
-    Context class for implementing trading strategies. 
-    
+    Context class for implementing trading strategies.
+
     Construction:
     - strategy:             Trading strategy class such as:
-                            crossoverTrading, zScoreTrading. 
+                            crossoverTrading, zScoreTrading.
     - plot_results:         bool to indicate whether to plot trading
                             activity.
 
     Example usage:
     ```
-    MA_type = "SMA" # simple moving average 
+    MA_type = "SMA" # simple moving average
     MAslow = 40 # slow moving average period
     MAfast = 10 # fast MA period
 
-    strategy = crossoverTrading(<asset df>, <asset sym>, MA_type, 
+    strategy = crossoverTrading(<asset df>, <asset sym>, MA_type,
                                 MAslow, fast_MA=MAfast)
     trader = backtest(strategy)
     trader.trade()
@@ -28,13 +28,11 @@ class backtest():
 
     def trade(self):
         """
-        Implements the stategies trading method. 
+        Implements the stategies trading method.
 
-        trade() method of each strategy stores returns in inputted 
-        df['returns']. See crossover_trading_strategy.py or 
-        zscore_trading_strategy.py for more details. 
+        trade() method of each strategy stores returns in inputted
+        df['returns']. See crossover_trading_strategy.py or
+        zscore_trading_strategy.py for more details.
         """
-        
-        self._strategy.trade(plot=self.plot_results)
 
-    
+        self._strategy.trade(plot=self.plot_results)
