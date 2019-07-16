067d811dd31dd51476ae494634a41241edfa62ff
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-07-16 12:47:37 +0100

    PEP8 formatting crossover_trading_strategy.py

--- Lib/crossover_trading_strategy.py
+++ Lib/crossover_trading_strategy.py
@@ -3,18 +3,19 @@ import pandas as pd
 
 from .abstract_MA_trading_strategy import movingAverageTrading
 
+
 class crossoverTrading(movingAverageTrading):
     """
-    A class that backtests a moving average based trading algorithm 
-    which attempts to capitalise on momentum and trends in the 
+    A class that backtests a moving average based trading algorithm
+    which attempts to capitalise on momentum and trends in the
     cryptocurrency market.
 
     Uses a longer (slower) moving average and a shorter (faster)
-    moving average. Will enter a long if faster MA crosses upwards 
+    moving average. Will enter a long if faster MA crosses upwards
     through slower MA and a short if it crosses downwards.
-    
-    Returns are stored under df['returns']. Refer to base class, 
-    movingAverageTrading for more details. 
+
+    Returns are stored under df['returns']. Refer to base class,
+    movingAverageTrading for more details.
 
     Initialisation:
     - df:               (pandas DataFrame) containing asset price
@@ -22,7 +23,7 @@ class crossoverTrading(movingAverageTrading):
     - asset_symbol:     (str) header of asset price history in df
     - fast_MA:          (int) period of shorter, faster MA
     - slow_MA:          (int) period of longer, slower MA
-    - MA_type:          (str) 'SMA' or 'EMA' for simple MA or 
+    - MA_type:          (str) 'SMA' or 'EMA' for simple MA or
                         exponential MA
     - trading_fee:      (double) fractional trading fee between 0 and 1
 
@@ -31,53 +32,51 @@ class crossoverTrading(movingAverageTrading):
     - Opening a long simultaneously closes any open shorts
     """
 
-    def __init__(self, df, asset_symbol, MA_type, 
+    def __init__(self, df, asset_symbol, MA_type,
                  slow_MA, fast_MA=1, trading_fee=0.0):
         args = (df, asset_symbol, MA_type, slow_MA)
-        kwargs = {"fast_MA" : fast_MA, "trading_fee" : trading_fee}
+        kwargs = {"fast_MA": fast_MA, "trading_fee": trading_fee}
         super(crossoverTrading, self).__init__(*args, **kwargs)
 
-    
     def plotTrading(self, opentimes, closetimes):
         """
-        Plots the executed trading. 
+        Plots the executed trading.
         2x1 subplots:
         subplot 1:          Asset price series w/ fast and slow MAs
         subplot 2:          Cumulative returns of trading
 
-        Notes: 
-        - plot has green and red verticle lines indicating 
-        opening longs and shorts respectively 
+        Notes:
+        - plot has green and red verticle lines indicating
+        opening longs and shorts respectively
         """
 
         t0 = self.MAs_period
         T = self.df.shape[0]
         plt.subplot(211)
         self.df.loc[t0:T, self.sym].plot(label=self.sym)
-        self.df.loc[t0:T, self.MAs_str].plot(label=self.MAs_str)        
+        self.df.loc[t0:T, self.MAs_str].plot(label=self.MAs_str)
         if self.MAf_period > 1:
             self.df.loc[t0:T, self.MAf_str].plot(label=self.MAf_str)
         [plt.axvline(x, c='g', lw=0.5, ls='--') for x in opentimes]
-        [plt.axvline(x, c='r', lw=0.5, ls='--') for x in closetimes] 
-        plt.ylabel('{}/BTC'.format(self.sym))        
+        [plt.axvline(x, c='r', lw=0.5, ls='--') for x in closetimes]
+        plt.ylabel('{}/BTC'.format(self.sym))
         plt.legend()
 
         plt.subplot(212)
         self.df.loc[t0:T, 'returns'].cumsum().plot()
         plt.ylabel('Returns')
         plt.xlabel('Hours')
-        plt.show()        
+        plt.show()
 
-    
     def trade(self, plot=False):
         """
         Executes all trades from the earliest value that the SMA can be
-        determined. 
-        
+        determined.
+
         Inputs:
         - plot:             (bool) optional arg to plot trading results
         """
-            
+
         longtimes = []
         shorttimes = []
 
@@ -89,7 +88,6 @@ class crossoverTrading(movingAverageTrading):
                 self.closePosition(t)
                 self.openPosition(t, 'L')
                 longtimes.append(t)
-
 
             if MAf_t < MAs_t and MAf_t_1 > MAs_t_1:
                 self.closePosition(t)
