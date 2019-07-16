045361204a99857add046455a8dcd10b8b676ae8
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-07-16 12:54:19 +0100

    PEP8 formatting file_loading_strategies.py

--- Lib/file_loading_strategies.py
+++ Lib/file_loading_strategies.py
@@ -7,14 +7,15 @@ from pandas.plotting import register_matplotlib_converters
 
 from .abstract_data_loading_strategy import dataLoadingStrat
 
+
 class fileLoadingRaw(dataLoadingStrat):
     """
-    concrete data loading strategy used for dataLoader class. 
-    Designed to load raw data stored from CryptoCompare api that as been 
+    concrete data loading strategy used for dataLoader class.
+    Designed to load raw data stored from CryptoCompare api that as been
     stored in json format
 
     Initialisation / construction:
-    - infile:               desired json file to load. json file has 
+    - infile:               desired json file to load. json file has
                             been saved using the related webLoading
                             class, that is file is of format:
                             { "asset": [
@@ -31,16 +32,20 @@ class fileLoadingRaw(dataLoadingStrat):
                                         ],
                             ...
                             }
-
+    - symbols:              list of symbols corresponding to data in
+                            infile.
+    - ticksize:             interval of datapoints in infile.
+                            "minute", "hour" or "day".
+    - outfile:              optional outfile to save df.
     """
 
     def __init__(self, infile, symbols, ticksize, outfile=False):
-        
+
         if infile.endswith('.json'):
             self._infile = infile
         else:
             raise ValueError("Infile must be json format")
-        
+
         if all(isinstance(symbol, str) for symbol in symbols):
             self._symbols = symbols
         else:
@@ -52,56 +57,52 @@ class fileLoadingRaw(dataLoadingStrat):
             self._freq = '1D'
         elif ticksize == 'minute':
             self._freq = '1T'
-        else: 
+        else:
             raise ValueError("Incompatible ticksize")
-        
+
         self._ticksize = ticksize
 
         if outfile and not outfile.endswith('.csv'):
             raise ValueError("outfile must be csv type")
-            
 
         self._outfile = outfile
 
-
     def get_data(self):
         """
-        Loads close prices of all assets in self._symbols for ticksize 
-        self._freq from raw data in self._infile by computing several 
+        Loads close prices of all assets in self._symbols for ticksize
+        self._freq from raw data in self._infile by computing several
         steps:
         1. First checks all data and get the earliest and latest
         timestamps
-        2. Creates a pandas dataframe which stores a list of datetimes 
+        2. Creates a pandas dataframe which stores a list of datetimes
         as an index from earliest timestamp to latest timestamp in steps
         of self._ticksize.
-        3. Then pulls associated close prices from raw json file and 
-        stores in dataframe. 
-        
+        3. Then pulls associated close prices from raw json file and
+        stores in dataframe.
+
         Outputs:
         - df:               (pandas DataFrame) holds close prices of all
                             assets in self._symbols along with
                             associated datetimes as index
 
-
         Notes:
-        - If there is no close prices associated with a given date, 
-        function takes price from associated w/ previous time. 
+        - If there is no close prices associated with a given date,
+        function takes price from associated w/ previous time.
         - This function is reasonably ineffcient as iterates over the
-        dataset twice. First to determine times and second to get data        
+        dataset twice. First to determine times and second to get data
         """
 
-        
         with open(self._infile) as json_file:
             raw_data = json.load(json_file)
-    
+
         now = datetime.now()
-        earliest_timestamp = (now - datetime(1970,1,1)).total_seconds()
+        earliest_timestamp = (now - datetime(1970, 1, 1)).total_seconds()
         latest_timestamp = 0
 
-        # determine the longest series in file to build dataframe 
+        # determine the longest series in file to build dataframe
         for symbol in self._symbols:
             asset = raw_data[symbol]
-            for i in range(len(asset)):     
+            for i in range(len(asset)):
                 if asset[i]['time'] < earliest_timestamp:
                     earliest_timestamp = asset[i]['time']
                 if asset[i]['time'] > latest_timestamp:
@@ -116,16 +117,17 @@ class fileLoadingRaw(dataLoadingStrat):
 
         for symbol in self._symbols:
             print(symbol)
-            df[symbol] = 0.0 # initilise asset row in df
-            asset_data = raw_data[symbol]          
+            df[symbol] = 0.0  # initilise asset row in df
+            asset_data = raw_data[symbol]
+            close_times = ([datetime.fromtimestamp(item['time'])
+                            for item in asset_data])
 
-            close_times = [datetime.fromtimestamp(item['time']) for item in asset_data]
-            if(self._ticksize=="day"):
+            if(self._ticksize == "day"):
                 close_times = [time.date() for time in close_times]
             close_prices = [item["close"] for item in asset_data]
 
             # Store data in dataframe
-            #-----------------------------------------------------------
+            # -----------------------------------------------------------------
             for i in range(df.shape[0]):
                 date = df.index[i]
                 if self._ticksize == "day":
@@ -137,13 +139,12 @@ class fileLoadingRaw(dataLoadingStrat):
                     # if no data at date, assume the previous date's data
                     if i > 0:
                         df[symbol][i] = df[symbol][i-1]
-            #-----------------------------------------------------------
+            # -----------------------------------------------------------------
 
             if self._outfile:
                 df.to_csv(self._outfile)
-        
-        return df
 
+        return df
 
 
 class fileLoadingDF(dataLoadingStrat):
@@ -159,15 +160,4 @@ class fileLoadingDF(dataLoadingStrat):
             raise ValueError("infile must be csv format")
 
     def get_data(self):
-        return pd.read_csv(self._infile)            
-        
-
-            
-            
-
-        
-        
-        
-        
-
-
+        return pd.read_csv(self._infile)
