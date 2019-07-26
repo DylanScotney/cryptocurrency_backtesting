d4980ea7adf33344a2e115110dd97de3bdeaab06
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-07-26 14:20:24 +0100

    Adding docstrings to expmovingAverage

--- Lib/exponential_moving_average_class.py
+++ Lib/exponential_moving_average_class.py
@@ -1,6 +1,16 @@
 from .simple_moving_average_class import simpleMovingAverage
 
 class expMovingAverage(simpleMovingAverage):
+    """
+    Class that handles the exponential moving average of a timeseries
+    Inherited from simpleMovingAverage class.
+
+    Initialisation:
+    - series:               pandas series
+    - period:               (int) period of moving average
+    - _MA:                  (pandas series) stores EMA series
+    - name:                 name of object - can be used for plot labels
+    """
 
     def __init__(self, series, period):
         
