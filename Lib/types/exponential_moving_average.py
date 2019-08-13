d800ce7e69285300dbb7d828246f5223ad3e469a
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-08-13 15:38:56 +0100

    Updating docstrings in expMovingAverage

--- Lib/types/exponential_moving_average.py
+++ Lib/types/exponential_moving_average.py
@@ -2,21 +2,34 @@ import pandas as pd
 
 from .simple_moving_average import simpleMovingAverage
 
+
 class expMovingAverage(simpleMovingAverage):
     """
     Class that handles the exponential moving average of a timeseries
     Inherited from simpleMovingAverage class.
 
     Initialisation:
-    - series:               pandas series
+    - series:               (pd series) of asset
     - period:               (int) period of moving average
-    - _MA:                  (pandas series) stores EMA series
-    - name:                 name of object - can be used for plot labels
+
+    Members:
+    - self._period:         (int) period of moving average
+    - self._MA:             (pd series, float) panda series of moving
+                            average values
+    - self.name:            (str) name of object. Primarily used for
+                            plot labels.
+
+    Notes:
+    - Requires series input is a pandas series
+
+    To Do:
+    - Generalise series input to take any type of list/array and convert
+    to pandas series on construction.
     """
 
-    def __init__(self, series, period):        
+    def __init__(self, series, period):
         if not isinstance(period, int) or period < 1:
-            raise ValueError("Period must be a positive int")            
+            raise ValueError("Period must be a positive int")
         if not isinstance(series, pd.Series):
             raise ValueError("Series must be a pandas series")
 
