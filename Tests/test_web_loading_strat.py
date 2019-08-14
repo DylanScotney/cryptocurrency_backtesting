31a6baaca62e84a219066844a521cc3657533a5e
Dylan Scotney <ucapdls@ucl.ac.uk>
2019-08-14 11:44:18 +0100

    Updating test_web_loading_strat

--- Tests/test_web_loading_strat.py
+++ Tests/test_web_loading_strat.py
@@ -4,8 +4,9 @@ import os.path
 import json
 import pandas as pd
 
-from ..Lib.web_loading_strategies import webLoading
+from ..Lib.data_loading.web_loading_strategies import webLoading
 
+api_key = 'ed65515376edee87d01ec914bebcd66d66f97cb4f0b7266f54a043b0966758b9'
 
 def test_initialisation():
     """
@@ -14,7 +15,7 @@ def test_initialisation():
 
     enddate = datetime(2019,1,1)
     expected_start_date = datetime(2018,12,31,23)
-    test = webLoading(['a'], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
+    test = webLoading(api_key, ['a'], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
 
     assert(test._symbols == ['a'])
     assert(test._ticksize == "hour")
@@ -32,7 +33,7 @@ def test_initialisation2():
 
     enddate = datetime(2019,1,1)
     expected_start_date = datetime(2018,12,31)
-    test = webLoading(['a'], "day", enddate, 1, "jsonfile.json", "csvfile.csv")
+    test = webLoading(api_key, ['a'], "day", enddate, 1, "jsonfile.json", "csvfile.csv")
 
     assert(test._symbols == ['a'])
     assert(test._ticksize == "day")
@@ -50,7 +51,7 @@ def test_initialisation3():
 
     enddate = datetime(2019,1,1)
     expected_start_date = datetime(2018,12,31,23,59)
-    test = webLoading(['a'], "minute", enddate, 1, "jsonfile.json", "csvfile.csv")
+    test = webLoading(api_key, ['a'], "minute", enddate, 1, "jsonfile.json", "csvfile.csv")
 
     assert(test._symbols == ['a'])
     assert(test._ticksize == "minute")
@@ -69,7 +70,7 @@ def test_initialisation_failure1():
     enddate = datetime(2019,1,1)
     symbols = ["not", 1, "all strings"]
     with raises(ValueError):
-        webLoading(symbols, "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
+        webLoading(api_key, symbols, "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
     
 
 def test_initialisation_failure2():
@@ -80,7 +81,7 @@ def test_initialisation_failure2():
     enddate = datetime(2019,1,1)
     ticksize = "non compatible ticksize"
     with raises(ValueError):
-        webLoading(['a'], ticksize , enddate, 1, "jsonfile.json", "csvfile.csv")
+        webLoading(api_key, ['a'], ticksize , enddate, 1, "jsonfile.json", "csvfile.csv")
 
 
 def test_initialisation_failure3():
@@ -90,7 +91,7 @@ def test_initialisation_failure3():
 
     enddate = "non compatible enddate"
     with raises(ValueError):
-        webLoading(['a'], "hour" , enddate, 1, "jsonfile.json", "csvfile.csv")
+        webLoading(api_key, ['a'], "hour" , enddate, 1, "jsonfile.json", "csvfile.csv")
 
 
 def test_initialisation_failure4():
@@ -102,11 +103,11 @@ def test_initialisation_failure4():
 
     lookback1 = 0
     with raises(ValueError):
-        webLoading(['a'], "hour" , enddate, lookback1, "jsonfile.json", "csvfile.csv")
+        webLoading(api_key, ['a'], "hour" , enddate, lookback1, "jsonfile.json", "csvfile.csv")
 
     lookback2 = 1.2
     with raises(ValueError):
-        webLoading(['a'], "hour" , enddate, lookback2, "jsonfile.json", "csvfile.csv")
+        webLoading(api_key, ['a'], "hour" , enddate, lookback2, "jsonfile.json", "csvfile.csv")
 
 
 def test_construct_url1():
@@ -116,7 +117,7 @@ def test_construct_url1():
 
 
     enddate = datetime(2019, 1, 1)
-    loader = webLoading(["ETH"], "hour", enddate, 1, "jsonfile.json",  "csvfile.csv")
+    loader = webLoading(api_key, ["ETH"], "hour", enddate, 1, "jsonfile.json",  "csvfile.csv")
     expected_url = ("https://min-api.cryptocompare.com/data/histohour?fsym=" +
                     "ETH&tsym=BTC&limit=1&api_key=ed65515376edee87d01ec914b" +
                     "ebcd66d66f97cb4f0b7266f54a043b0966758b9")
@@ -131,7 +132,7 @@ def test_construct_url2():
     """
 
     enddate = datetime(2019, 1, 1)
-    loader = webLoading(["ETH"], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
+    loader = webLoading(api_key, ["ETH"], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
     expected_url = ("https://min-api.cryptocompare.com/data/histohour?fsym=" +
                     "ETH&tsym=BTC&limit=1&toTs=timestamp&api_key=ed65515376" +
                     "edee87d01ec914bebcd66d66f97cb4f0b7266f54a043b0966758b9")
@@ -148,7 +149,7 @@ def test_pull_data():
     enddate = datetime(2019, 1, 1)
     with open ("expected_pull_data.json") as json_file:
         expected_response = json.load(json_file)
-    loader = webLoading(["ETH"], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
+    loader = webLoading(api_key, ["ETH"], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
     response = loader._pull_data("ETH", 1, datetime.timestamp(enddate))    
 
     assert(response == expected_response)
@@ -160,7 +161,7 @@ def test_get_data():
     """
 
     enddate = datetime(2019, 1, 1)
-    loader = webLoading(["ETH"], "hour", enddate, 1, "expected_pull_data.json", "csvfile.csv")
+    loader = webLoading(api_key, ["ETH"], "hour", enddate, 1, "jsonfile.json", "csvfile.csv")
     df = loader.get_data()
     df = df.reset_index()
     expected_df = pd.DataFrame({'date': [datetime(2018,12,31,23), 
